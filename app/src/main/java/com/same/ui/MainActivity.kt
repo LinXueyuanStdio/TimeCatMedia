8232cc78476b38b3311620442cfc68b4808ddff0
linxueyuanstdio <761516186@qq.com>
2021-05-30 19:26:20 +0800

    save

--- app/src/main/java/com/same/ui/MainActivity.kt
+++ app/src/main/java/com/same/ui/MainActivity.kt
@@ -2,25 +2,117 @@ package com.same.ui
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
+import android.os.Looper
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.HorizontalScrollView
 import android.widget.LinearLayout
-import com.timecat.identity.readonly.RouterHub
-import com.xiaojinzi.component.impl.*
+import com.google.android.material.chip.Chip
+import com.timecat.fake.media.R
+import com.timecat.layout.ui.layout.*
 
 class MainActivity : Activity() {
+
+    lateinit var commandLinear: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val linearLayout = LinearLayout(this)
         linearLayout.orientation = LinearLayout.VERTICAL
-        linearLayout.addView(createButton("登录", RouterHub.LOGIN_LoginActivity))
-        linearLayout.addView(createButton("添加插件", RouterHub.USER_AddPluginAppActivity))
-        linearLayout.addView(createButton("添加动态", RouterHub.USER_AddMomentActivity))
-        linearLayout.addView(createButton("背包", RouterHub.USER_BagActivity))
+
+        val scrollView = HorizontalScrollView(this).apply {
+            clipChildren = true
+            clipToPadding = false
+            layout_width = match_parent
+            layout_height = wrap_content
+            margin_top = 50
+            margin_start = 50
+            margin_end = 50
+            commandLinear = LinearLayout(context)
+            commandLinear.orientation = LinearLayout.HORIZONTAL
+            addView(commandLinear)
+
+            for (i in 0..10) {
+                commandLinear.addView(Chip(context).apply {
+                    margin_end = 10
+                    bindAnimation(this)
+                    text = "chip $i"
+                })
+            }
+        }
+        val frameLayout = FrameLayout(this)
+        frameLayout.clipChildren = true
+        frameLayout.clipToPadding = true
+        frameLayout.addView(scrollView)
+        linearLayout.addView(frameLayout)
+        linearLayout.addView(createButton("启动动画") {
+            resetAnimation()
+            commandLinear.removeAllViews()
+            for (i in 0..10) {
+                commandLinear.addView(Chip(this).apply {
+                    margin_end = 10
+                    bindAnimation(this)
+                    text = "chip $i"
+                })
+            }
+        })
         setContentView(linearLayout)
+    }
+
+
+    //region anim add chip
+    protected open val isAnimationEnabled: Boolean
+        get() = true
+    private var isAnimating = false
+
+    private var animationStartOffset = 0
+
+    private val stopAnimationHandler = Handler(Looper.getMainLooper())
+    private val stopAnimationRunnable = Runnable { stopAnimation() }
+
+    fun bindAnimation(holder: Chip) {
+        holder.clearAnimation()
+        if (isAnimating) {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.item_on_load_hori)
+                .apply { startOffset = animationStartOffset.toLong() }
+            animationStartOffset += ANIMATION_STAGGER_MILLIS
+            holder.startAnimation(animation)
+            postStopAnimation()
+        }
+    }
+
+    private fun stopAnimation() {
+        stopAnimationHandler.removeCallbacks(stopAnimationRunnable)
+        isAnimating = false
+        animationStartOffset = 0
+    }
+
+    private fun postStopAnimation() {
+        stopAnimationHandler.removeCallbacks(stopAnimationRunnable)
+        stopAnimationHandler.post(stopAnimationRunnable)
+    }
+
+    private fun clearAnimationOfSubView() {
+        stopAnimation()
+        for (index in 0 until commandLinear.childCount) {
+            commandLinear.getChildAt(index).clearAnimation()
+        }
+    }
+
+    private fun resetAnimation() {
+        clearAnimationOfSubView()
+        isAnimating = isAnimationEnabled
+    }
+    //endregion
+
+
+    companion object {
+        private const val ANIMATION_STAGGER_MILLIS = 20
+        private const val TRANSLATE_DURATION_MILLIS = 200
     }
 
     private fun createButton(name: String, path: String): Button {
@@ -45,14 +137,5 @@ class MainActivity : Activity() {
     }
 
     private fun go(path: String) {
-        Router.with().hostAndPath(path)
-            .forward(object : Callback {
-                override fun onSuccess(result: RouterResult) {}
-                override fun onEvent(successResult: RouterResult?, errorResult: RouterErrorResult?) {}
-                override fun onCancel(originalRequest: RouterRequest?) {}
-                override fun onError(errorResult: RouterErrorResult) {
-                    Log.e("ui", errorResult.error.toString())
-                }
-            })
     }
 }
